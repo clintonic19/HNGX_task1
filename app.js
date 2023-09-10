be0addf0c76287f7dffd922e25e55b7b2793aa30
clintonic19 <44939329+clintonic19@users.noreply.github.com>
2023-09-10 22:26:09 +0100

    Update app.js
    update on time

--- app.js
+++ app.js
@@ -26,8 +26,9 @@ const currentDate = new Date();
       const hours = currentDate.getUTCHours().toString().padStart(2, '0');
       const minutes = currentDate.getUTCMinutes().toString().padStart(2, '0');
       const seconds = currentDate.getUTCSeconds().toString().padStart(2, '0');
+      const miliseconds = currentDate.getUTCMilliseconds(123);
       // Create the formatted UTC time string
-      const currentUTC = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`
+      const currentUTC = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}:${miliseconds}Z`
 
 app.get('/api', (req, res) => {
     try {
