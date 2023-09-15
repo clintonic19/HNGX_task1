b04f4046b3d0d52b00b8c18b66209cedc5d52c1e
clinton clinton <clintonictendency@gmail.com>
2023-09-15 10:17:48 +0100

    updated app.js file

--- app.js
+++ app.js
@@ -24,38 +24,38 @@ app.get('/', (req, res) => {
     }
 });
 
-app.get('/api', (req, res) => {
-    try {
-        const { slack_name, track } = req.query;
+// app.get('/api', (req, res) => {
+//     try {
+//         const { slack_name, track } = req.query;
 
-        // Create the formatted UTC time string
-        const currentDate = new Date();
-        const year = currentDate.getUTCFullYear();
-        const month = (currentDate.getUTCMonth() + 1).toString().padStart(2, '0');
-        const day = currentDate.getUTCDate().toString().padStart(2, '0');
-        const hours = currentDate.getUTCHours().toString().padStart(2, '0');
-        const minutes = currentDate.getUTCMinutes().toString().padStart(2, '0');
-        const seconds = currentDate.getUTCSeconds().toString().padStart(2, '0');
+//         // Create the formatted UTC time string
+//         const currentDate = new Date();
+//         const year = currentDate.getUTCFullYear();
+//         const month = (currentDate.getUTCMonth() + 1).toString().padStart(2, '0');
+//         const day = currentDate.getUTCDate().toString().padStart(2, '0');
+//         const hours = currentDate.getUTCHours().toString().padStart(2, '0');
+//         const minutes = currentDate.getUTCMinutes().toString().padStart(2, '0');
+//         const seconds = currentDate.getUTCSeconds().toString().padStart(2, '0');
 
-        // Create the formatted UTC time string
-        const currentUTC = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`
+//         // Create the formatted UTC time string
+//         const currentUTC = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`
 
-        const response = {
-            slack_name,
-            track,
-            current_day: currentDate.toLocaleDateString('en-US', { weekday: 'long' }),
-            utc_time: currentUTC,
-            github_file_url: 'https://github.com/clintonic19/HNGX_task1/blob/master/app.js',
-            github_repo_url: "https://github.com/clintonic19/HNGX_task1",
-            status_code: 200,
-        };
-        res.json(response);
-    }
-    catch (error) {
-        console.error('An error occurred:', error || error.message);
-        res.status(500).json({ error: 'Internal Server Error' || error.message });
-    }
-});
+//         const response = {
+//             slack_name,
+//             track,
+//             current_day: currentDate.toLocaleDateString('en-US', { weekday: 'long' }),
+//             utc_time: currentUTC,
+//             github_file_url: 'https://github.com/clintonic19/HNGX_task1/blob/master/app.js',
+//             github_repo_url: "https://github.com/clintonic19/HNGX_task1",
+//             status_code: 200,
+//         };
+//         res.json(response);
+//     }
+//     catch (error) {
+//         console.error('An error occurred:', error || error.message);
+//         res.status(500).json({ error: 'Internal Server Error' || error.message });
+//     }
+// });
 
 //POST USERS INTO THE DB
 app.post('/api', async (req, res) => {
